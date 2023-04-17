0061e45d2624a10ecf31f4c3bb2e920b13953aa2
anastakozz <shvedagon@gmail.com>
2023-04-18 00:49:29 +0300

    feat: add min and max

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -115,13 +115,26 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.start) {
+      return false;
+    }
+
+    let node = this.start;
+    while (node.left){
+      node = node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.start) {
+      return false;
+    }
+    let node = this.start;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
